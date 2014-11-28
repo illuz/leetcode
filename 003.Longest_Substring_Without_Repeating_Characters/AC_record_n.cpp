90c8eef1860182ec79668d33bbbe74c80221ba16
illuz <iilluzen@gmail.com>
2014-11-28 08:28:38 +0800

    Longest Substring Without Repeating Characters

---
+++ 003.Longest_Substring_Without_Repeating_Characters/AC_record_n.cpp
@@ -0,0 +1,40 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_record_n.cpp
+*  Create Date: 2014-11-28 08:11:12
+*  Descripton:  record the last appeared num's position, O(n)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 300;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+       int maxlen = 0, left = 0;
+	   int sz = s.length();
+	   int prev[N];
+	   memset(prev, -1, sizeof(prev));
+
+	   for (int i = 0; i < sz; i++) {
+		   if (prev[s[i]] >= left) {
+			   left = prev[s[i]] + 1;
+		   }
+		   prev[s[i]] = i;
+		   maxlen = max(maxlen, i - left + 1);
+	   }
+	   return maxlen;
+    }
+};
+
+int main() {
+	Solution a;
+	string s;
+	while (cin >> s) {
+		cout << a.lengthOfLongestSubstring(s) << endl;
+	}
+	return 0;
+}
+
