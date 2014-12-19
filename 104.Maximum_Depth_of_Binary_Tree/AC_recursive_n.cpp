6f7f740a84ee8b4dedc391e607bb8a616fc1e481
illuz <iilluzen@gmail.com>
2014-12-19 08:50:53 +0800

    Maximum Depth of Binary Tree

---
+++ 104.Maximum_Depth_of_Binary_Tree/AC_recursive_n.cpp
@@ -0,0 +1,35 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_recursive_n.cpp
+*  Create Date: 2014-12-19 08:48:06
+*  Descripton:  recursive 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 0;
+
+
+// Definition for binary tree
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+		if (root == NULL)
+			return 0;
+		return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+
+int main() {
+
+	return 0;
+}
+
