10b92d02a780af8cef6b467f69e037f6963326b2
illuz <iilluzen@gmail.com>
2014-12-24 11:23:55 +0800

    Maximum Subarray

---
+++ 052.Maximum_Subarray/AC_dp_n.cpp
@@ -0,0 +1,36 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_dp_n.cpp
+*  Create Date: 2014-12-24 11:16:17
+*  Descripton:  dp
+*               http://blog.csdn.net/hcbbt/article/details/10454947
+*               the way 4
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 0;
+
+class Solution {
+public:
+    int maxSubArray(int A[], int n) {
+		if (n == 0)
+			return 0;
+		int sum = A[0], mmax = A[0];
+		for (int i = 1; i < n; i++) {
+			if (sum < 0)
+				sum = A[i];
+			else
+				sum += A[i];
+			mmax = max(mmax, sum);
+		}
+		return mmax;
+    }
+};
+
+int main() {
+
+	return 0;
+}
+
