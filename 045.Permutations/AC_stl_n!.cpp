1ebfdd7154cd7e4ec2a1888e8dc97c26a820bb7a
illuz <iilluzen@gmail.com>
2014-12-24 13:25:18 +0800

    Permutations

---
+++ 045.Permutations/AC_stl_n!.cpp
@@ -0,0 +1,29 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_stl_n!.cpp
+*  Create Date: 2014-12-24 11:39:42
+*  Descripton:  use STL next_permutation(). 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 0;
+
+class Solution {
+public:
+	vector<vector<int> > permute(vector<int> &num) {
+		sort(num.begin(), num.end());
+		vector<vector<int> > res;
+		do {
+			res.push_back(num);
+		} while (next_permutation(num.begin(), num.end()));
+		return res;
+	}
+};
+
+int main() {
+
+	return 0;
+}
+
