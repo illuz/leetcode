8fbfee739d564b9e121ea1d8b80a19c1aaa7b5b5
illuz <iilluzen@gmail.com>
2014-12-31 01:03:36 +0800

    Pow

---
+++ 049.Pow(x,_n)/AC_simulation_n.cpp
@@ -0,0 +1,39 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_simulation_n.cpp
+*  Create Date: 2014-12-31 00:10:05
+*  Descripton:  simulation
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 0;
+
+class Solution {
+public:
+    double pow(double x, int n) {
+		double res = 1;
+		if (n < 0) {
+			n = -n;
+			x = 1.0 / x;
+		}
+		while (n > 0) {
+			if (n % 2)
+				res *= x;
+			n >>= 1;
+			x *= x;
+		}
+		return res;
+    }
+};
+
+int main() {
+	int n;
+	double x;
+	Solution s;
+	while (cin >> x >> n)
+		cout << s.pow(x, n) << endl;
+	return 0;
+}
+
