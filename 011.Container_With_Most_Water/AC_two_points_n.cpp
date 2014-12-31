0fbda5df1e52c3d9e392ba82041348ca44be23f0
illuz <iilluzen@gmail.com>
2014-12-31 11:17:15 +0800

    Container With Most Water

---
+++ 011.Container_With_Most_Water/AC_two_points_n.cpp
@@ -0,0 +1,34 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_two_points_n.cpp
+*  Create Date: 2014-12-31 11:13:00
+*  Descripton:  left point and right point 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 0;
+
+class Solution {
+public:
+    int maxArea(vector<int> &height) {
+		int lpoint = 0, rpoint = height.size() - 1;
+		int area = 0;
+		while (lpoint < rpoint) {
+			area = max(area, min(height[lpoint], height[rpoint]) *
+					(rpoint - lpoint));
+			if (height[lpoint] > height[rpoint])
+				rpoint--;
+			else
+				lpoint++;
+		}
+		return area;
+    }
+};
+
+int main() {
+
+	return 0;
+}
+
