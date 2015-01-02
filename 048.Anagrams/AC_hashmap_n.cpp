1b5eb5370409d57f7fc6f0509df83785c86c9022
illuz <iilluzen@gmail.com>
2015-01-02 11:31:21 +0800

    Anagrams

---
+++ 048.Anagrams/AC_hashmap_n.cpp
@@ -0,0 +1,37 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_hashmap_nlog.cpp
+*  Create Date: 2015-01-02 11:15:47
+*  Descripton:  hashmap, use map to record the sorted string
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 0;
+
+class Solution {
+public:
+	vector<string> anagrams(vector<string> &strs) {
+		vector<string> ans;
+		map<string, vector<int> > rec;
+		for (int i = 0; i < strs.size(); i++) {
+			string str = strs[i];
+			sort(str.begin(), str.end());
+			rec[str].push_back(i);
+		}
+		for (auto &it : rec) {
+			if (it.second.size() > 1) {
+				for (auto &ind : it.second)
+					ans.push_back(strs[ind]);
+			}
+		}
+		return ans;
+	}
+};
+
+int main() {
+
+	return 0;
+}
+
