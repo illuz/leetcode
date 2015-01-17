7b1a7ce4fb3825aced5347a5a65ba8c9f2faaabc
illuz <iilluzen@gmail.com>
2015-01-17 11:02:53 +0800

    use STL to solve Permutation

---
+++ 030.Next_Permutation/AC_STL_1.cpp
@@ -0,0 +1,24 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_STL_1.cpp
+*  Create Date: 2015-01-17 11:02:10
+*  Descripton:  Use next_permutation
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 0;
+
+class Solution {
+public:
+    void nextPermutation(vector<int> &num) {
+        next_permutation(num.begin(), num.end());
+    }
+};
+
+int main() {
+
+	return 0;
+}
+
---
+++ 059.Permutation_Sequence/TLE_STL_n!.cpp
@@ -0,0 +1,29 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        TLE_STL_n!.cpp
+*  Create Date: 2015-01-17 10:17:40
+*  Descripton:  Use next_permutation 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 0;
+
+class Solution {
+public:
+	string getPermutation(int n, int k) {
+		string str;
+		for (int i = 0; i < n; i++)
+			str.push_back(i + '1');
+		while (k--)
+			next_permutation(str.begin(), str.end());
+		return str;
+	}
+};
+
+int main() {
+
+	return 0;
+}
+
