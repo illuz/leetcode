72559e5e1563c91c8b27b14f388b7253aab88545
illuz <iilluzen@gmail.com>
2015-03-06 11:33:01 +0800

    027. Remove Element (Easy)

--- solutions/027.Remove_Element/AC_simulate_n.cpp
+++ solutions/027.Remove_Element/AC_simulate_n.cpp
@@ -13,16 +13,16 @@ const int N = 0;
 class Solution {
 public:
     int removeElement(int A[], int n, int elem) {
-		int ret = 0;
-		for (int i = 0; i < n; i++)
-			if (A[i] != elem)
-				A[ret++] = A[i];
-		return ret;
+        int ret = 0;
+        for (int i = 0; i < n; i++)
+            if (A[i] != elem)
+                A[ret++] = A[i];
+        return ret;
     }
 };
 
 int main() {
 
-	return 0;
+    return 0;
 }
 
---
+++ solutions/027.Remove_Element/readme.md
@@ -0,0 +1,13 @@
+## 027. Remove Element (Easy)
+
+### **链接**：
+题目：https://oj.leetcode.com/problems/remove-element/  
+代码(github)：https://github.com/illuz/leetcode
+
+### **题意**：
+删除一个数组里值为 elem 的所有数。
+
+### **分析**：
+用两个指针，一个为可放位置的指针，一个为扫描指针。  
+因为不难，Java 和 Python 的做法都和 C++ 一样，这里就不给出了。
+
