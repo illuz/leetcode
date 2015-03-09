9ad8724871d668379a6a5da7c500661cf55fc46c
illuz <iilluzen@gmail.com>
2015-03-09 10:28:50 +0800

    108.Convert_Sorted_Array_to_Binary_Search_Tree

---
+++ solutions/108.Convert_Sorted_Array_to_Binary_Search_Tree/AC_simulation_n.cpp
@@ -0,0 +1,42 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_simulation_n.cpp
+*  Create Date: 2015-03-09 10:21:14
+*  Descripton:  Just simulation.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 0;
+
+// Definition for binary tree
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+private:
+    TreeNode *dfs(vector<int> &num, int left, int right) {
+        if (left > right)
+            return NULL;
+
+        int mid = (left + right) / 2;
+        TreeNode *root = new TreeNode(num[mid]);
+        root->left = dfs(num, left, mid - 1);
+        root->right = dfs(num, mid + 1, right);
+    }
+public:
+    TreeNode *sortedArrayToBST(vector<int> &num) {
+        return dfs(num, 0, num.size() - 1);
+    }
+};
+
+int main() {
+
+	return 0;
+}
+
---
+++ solutions/108.Convert_Sorted_Array_to_Binary_Search_Tree/readme.md
@@ -0,0 +1,12 @@
+## 108. Convert Sorted Array to Binary Search Tree (Medium)
+
+### **链接**：
+题目：https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/  
+代码(github)：https://github.com/illuz/leetcode
+
+### **题意**：
+为一个排过序的数组建一棵平衡的 BST。
+
+### **分析**：
+要平衡，只要取中间的那个数做根就行了。递归下去即可。
+
