b4ddaba9746e88d462ad84b33a7591c74845f5a5
illuz <iilluzen@gmail.com>
2015-03-14 21:11:44 +0800

    188

---
+++ solutions/188.Best_Time_to_Buy_and_Sell_Stock_IV/AC_dp_nk.cpp
@@ -0,0 +1,48 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_dp_nk.cpp
+*  Create Date: 2015-03-14 20:59:20
+*  Descripton:  Just like the 123.III's O(1) space version.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 0;
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int> &prices) {
+        int n = prices.size();
+        if (k == 0 || n < 2)
+            return 0;
+
+        if (k > n / 2) {
+            // like the 122.II version
+            int ret = 0;
+            for (int i = 1; i < n; ++i)
+                if (prices[i] > prices[i - 1])
+                    ret += prices[i] - prices[i - 1];
+            return ret;
+        }
+
+        vector<int> buy(k, INT_MIN);
+        vector<int> sell(k, 0);
+        for (int i = 0; i < n; ++i) {
+            for (int j = k - 1; j >= 0; --j) {
+                sell[j] = max(sell[j], buy[j] + prices[i]);
+                if (j == 0)
+                    buy[j] = max(buy[j], -prices[i]);
+                else
+                    buy[j] = max(buy[j], sell[j - 1] - prices[i]);
+            }
+        }
+        return sell[k - 1];
+    }
+};
+
+int main() {
+
+	return 0;
+}
+
---
+++ solutions/188.Best_Time_to_Buy_and_Sell_Stock_IV/readme.md
@@ -0,0 +1,13 @@
+## 188. Best Time to Buy and Sell Stock IV (Hard)
+
+### **链接**：
+题目：https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/  
+代码(github)：https://github.com/illuz/leetcode
+
+### **题意**：
+给一个数组，prices[i] 表示第 i 天的交易值，也就是你在这天买入或卖出的交易值。  
+你可以买入及卖出**最多k轮**，不过你一个时间只能拥有一个股票，求最大盈利。
+
+### **分析**：
+
+只要理解了 [`123. Best Time to Buy and Sell Stock III `](https://github.com/illuz/leetcode/tree/master/solutions/123.Best_Time_to_Buy_and_Sell_Stock_III) 的那个 O(1) 空间的算法，这个也可以那样变形过来。
