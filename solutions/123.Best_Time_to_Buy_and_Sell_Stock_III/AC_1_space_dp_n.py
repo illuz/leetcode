1b66005ffa59ce1221464381af9102763ac0f2e0
illuz <iilluzen@gmail.com>
2015-03-14 11:10:13 +0800

    fix

--- solutions/123.Best_Time_to_Buy_and_Sell_Stock_III/AC_1_space_dp_n.py
+++ solutions/123.Best_Time_to_Buy_and_Sell_Stock_III/AC_1_space_dp_n.py
@@ -7,3 +7,19 @@
 # Descripton:  
 
 
+class Solution:
+    # @param prices, a list of integer
+    # @return an integer
+    def maxProfit(self, prices):
+        buy1, buy2 = -(1<<31), -(1<<31)
+        sell1, sell2 = 0, 0
+
+        for i in prices:
+            sell2 = max(sell2, buy2 + i)    # max money after sell 2
+            buy2 = max(buy2, sell1 - i)     # max money after buy 2
+            sell1 = max(sell1, buy1 + i)    # max money after sell 1
+            buy1 = max(buy1, -i)            # max money after buy 1
+
+        return sell2
+
+
