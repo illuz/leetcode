30355bbc6a7c69d30208f41171e6d1b9e11fbca3
illuz <iilluzen@gmail.com>
2015-04-23 22:48:00 +0800

    203 python solution

---
+++ solutions/203.Remove_Linked_List_Elements/AC_simulation_n.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author:      illuz <iilluzen[at]gmail.com>
+# File:        AC_simulation_n.py
+# Create Date: 2015-04-23 22:47:24
+# Usage:       AC_simulation_n.py 
+# Descripton:  
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @param {integer} val
+    # @return {ListNode}
+    def removeElements(self, head, val):
+        dummy = ListNode(0)
+        dummy.next = head
+        cur = dummy
+        while cur.next:
+            if cur.next.val == val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return dummy.next
