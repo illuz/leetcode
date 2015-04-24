dcebb1f7547313d2b85f84d18f0462d27185a3ea
illuz <iilluzen@gmail.com>
2015-04-24 23:04:39 +0800

    add lazy solution 154

---
+++ solutions/154.Find_Minimum_in_Rotated_Sorted_Array_II/AC_lazy_n.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author:      illuz <iilluzen[at]gmail.com>
+# File:        AC_lazy_n.py
+# Create Date: 2015-04-24 23:04:10
+# Usage:       AC_lazy_n.py 
+# Descripton:  
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def findMin(self, nums):
+        return min(nums)
