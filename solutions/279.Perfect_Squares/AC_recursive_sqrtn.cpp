87430d04efb0518ccbc3464347a460afe912c222
illuz <iilluzen@gmail.com>
2015-10-26 11:02:21 +0800

    Solve 279 with C++ (Recursive + Momorization).

---
+++ solutions/279.Perfect_Squares/AC_recursive_sqrtn.cpp
@@ -0,0 +1,39 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_recursive_n2.cpp
+*  Create Date: 2015-10-26 10:11:03
+*  Descripton:  recursiev + memorization
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 1010;
+
+class Solution {
+public:
+    int helper(int n, vector<int> &result) {
+        if (result[n] != 0)
+            return result[n];
+        int begin = int(sqrt(n));
+        if (n == begin * begin)
+            return 1;
+        int res = n;
+        for (int i = begin; i >= 1; --i) {
+            res = min(res, 1 + helper(n - i * i, result));
+        }
+        return result[n] = res;
+    }
+    int numSquares(int n) {
+        vector<int> result(n + 1);
+        return helper(n, result);
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.numSquares(12) << endl;
+    cout << s.numSquares(13) << endl;
+    return 0;
+}
+
