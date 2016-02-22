4d1e214ac9ae16a912c155c414933808fa185be2
illuz <iilluzen@gmail.com>
2016-02-23 01:23:31 +0800

    Solve 326

---
+++ solutions/326.Power_of_Three/AC_math_1.cpp
@@ -0,0 +1,25 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_math_1.cpp
+*  Create Date: 2016-02-23 01:18:19
+*  Descripton:  i
+*/
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+const int N = 0;
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return fmod(log10(n)/log10(3), 1)==0;
+    }
+};
+
+int main() {
+
+    return 0;
+}
+
---
+++ solutions/326.Power_of_Three/AC_normal_logn.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        while (n >= 3) {
+            if (n % 3 != 0) return false;
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
