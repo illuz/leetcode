ae5c47b86bdf8d351b22ad3117ef23f7682d998f
illuz <iilluzen@gmail.com>
2016-02-22 19:57:40 +0800

    Solve 292 Nim Game

---
+++ solutions/292.Nim_Game/AC_nim_1.cpp
@@ -0,0 +1,24 @@
+/*
+*  Author:      illuz <iilluzen[at]gmail.com>
+*  File:        AC_nim_1.cpp
+*  Create Date: 2016-02-22 19:44:50
+*  Descripton:   
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 0;
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4 != 0;
+    }
+};
+
+int main() {
+
+    return 0;
+}
+
